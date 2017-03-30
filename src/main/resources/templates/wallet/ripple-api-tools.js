350666d33998e79ee61e496e09abd357d32453c1
mougel <alix@ledger.fr>
2017-03-30 16:47:44 +0200

    js side : implementation for possible api initialisation call

--- src/main/resources/templates/wallet/ripple-api-tools.js
+++ src/main/resources/templates/wallet/ripple-api-tools.js
@@ -1,36 +1,35 @@
 
 const {RippleAPI} = require('ripple-lib');
 
-const api = new RippleAPI({
-  server: 'wss://s1.ripple.com' // Public rippled server hosted by Ripple, Inc.
-});
-api.on('error', (errorCode, errorMessage) => {
-  console.log(errorCode + ': ' + errorMessage);
-});
-api.on('connected', () => {
-  console.log('connected');
-});
-api.on('disconnected', (code) => {
-  // code - [close code](https://developer.mozilla.org/en-US/docs/Web/API/CloseEvent) sent by the server
-  // will be 1000 if this was normal closure
-  console.log('disconnected, code:', code);
-});
-api.connect().then(() => {
-  /* insert code here */
-}).then(() => {
-  return api.disconnect();
-}).catch(console.error);
+window.addEventListener('message', onMessage);
 
-
-window.addEventListener('message', function(event) {
-    var command = event.data.command;
-    var name = event.data.name || 'hello';
-    switch(command) {
-      case 'render':
-        event.source.postMessage({
-          name: name,
-          html: templates[name](event.data.context)
-        }, event.origin);
-        break;
+function onMessage(event) {
+    var method = event.data.method
+    var call_id = event.data.call_id
+    var parameters = event.data.parameters
+    switch(method) {
+        case "set_option":
+            response = createAPI(parameters);
+            break;
     }
-});
+    var toScala = {call_id = event.data.call_id, response=response}
+    event.source.postMessage(toScala, event.origin)
+}
+
+function createAPI(options){
+    const api = new RippleAPI(options);
+    api.on('error', (errorCode, errorMessage) => {
+      console.log(errorCode + ': ' + errorMessage);
+    });
+    api.on('connected', () => {
+      console.log('connected');
+    });
+    api.on('disconnected', (code) => {
+      // code - [close code](https://developer.mozilla.org/en-US/docs/Web/API/CloseEvent) sent by the server
+      // will be 1000 if this was normal closure
+      console.log('disconnected, code:', code);
+    });
+    api.connect().catch(console.error);
+    var response = {connected=true}
+    return response
+}
