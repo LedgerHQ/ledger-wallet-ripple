565a94044780c5463f4f059407ba9eb1069f22e1
mougel <alix@ledger.fr>
2017-05-04 15:17:45 +0200

    Adding facade for autoupdate

---
+++ src/main/resources/libs/autoupdater.js
@@ -0,0 +1,2 @@
+const nwAutoupdate = require( "nw-autoupdater" );
+const os = require("os");
---
+++ src/main/scala/autoupdater/ApiUpdateRestClient.scala
@@ -0,0 +1,19 @@
+package autoupdater
+
+import co.ledger.wallet.core.net.HttpClient
+
+import scala.concurrent.Future
+
+/**
+  * Created by alix on 5/4/17.
+  */
+class ApiUpdateRestClient(http: HttpClient) {
+
+  def lastVersion(os: String): Future[String] = {
+    val request = http.get(s"/api/resolve?platform=$os&channel=stable")
+    request.json.map({
+      case (json, _) =>
+        json.getString("tag")
+    })
+  }
+}
